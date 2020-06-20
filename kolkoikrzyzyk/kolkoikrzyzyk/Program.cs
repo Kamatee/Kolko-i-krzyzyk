1a346f008ef30021afd2f82b08a82ed276df80f5
kamatee <kociara33399@gmail.com>
2020-06-20 19:39:54 +0200

    v0.2
    Początek planszy

--- kolkoikrzyzyk/kolkoikrzyzyk/Program.cs
+++ kolkoikrzyzyk/kolkoikrzyzyk/Program.cs
@@ -6,7 +6,52 @@ namespace kolkoikrzyzyk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+            char[,] board = new char[3, 3];
+            Initialize(board);
+
+            
+
+            Print(board);
+
+            Console.Write("Wybierz rząd: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Wybierz kolumnę: ");
+            int col = Convert.ToInt32(Console.ReadLine());
+
+            board[row, col] = 'X';
+
+            Print(board);
+   
+        }
+
+
+        
+        static void Initialize(char[,] board)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+        }
+
+        static void Print(char[,] board)
+        {
+            Console.WriteLine("  | 0 | 1 | 2 |");
+            for (int row = 0; row < 3; row++)
+            {
+                Console.Write(row + " | ");
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write(board[row, col]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
