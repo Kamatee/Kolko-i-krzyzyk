7f8855d1957c7912e64f168a152c29d3b474fd51
kamatee <kociara33399@gmail.com>
2020-06-20 17:07:21 +0200

    v0.1
    Start projektu

---
+++ kolkoikrzyzyk/kolkoikrzyzyk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kolkoikrzyzyk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
